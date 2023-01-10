cd19c908c6b30bcd1c4aba84a9fc377d517255de
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-01-10 15:27:00 +0900

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/base.scss';
+
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
